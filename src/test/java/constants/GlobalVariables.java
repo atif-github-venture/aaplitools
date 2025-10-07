38bcffaccebe91ef815774817310c761d0bb9838
Atif Ahmed <atif.project@gmail.com>
2025-10-07 10:28:04 -0400

    Update GlobalVariables.java

--- src/test/java/constants/GlobalVariables.java
+++ src/test/java/constants/GlobalVariables.java
@@ -2,10 +2,10 @@ package constants;
 
 public class GlobalVariables {
 
-	public static String baseFabricURL="http://flagship.lbidts.com/app?redirect=true";
+	public static String baseFabricURL="http://url.com/app?redirect=true";
 	public static String visualUrlFilePath="./src/test/resources/data/visualurl.yml";
 	public static String geckoDriverPath="./src/test/resources/drivers/geckodriver";
-	public static String appliToolsKey = "1ZP3f1bdQ8qmGbNZXmQCvvA105PXvygRpe981017KgNCADs110";
+	public static String appliToolsKey = "key";
 	public static int height = 600;
 	public static int width = 800;
 
